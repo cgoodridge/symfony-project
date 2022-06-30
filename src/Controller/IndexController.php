9b0d3aa4a06311e573e77af5358758f8727c2776
cgoodridge <cag45@live.com>
2022-06-30 16:44:07 -0400

    Initial Commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends AbstractController {
+
+    public function home() {
+        return new Response()
+    }
+}
